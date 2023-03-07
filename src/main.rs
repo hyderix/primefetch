ac0ae7f797c1d2b31c0ac8efb7e23b9bf2c602a7
Eric Thorburn <tabulateyard962@gmail.com>
2023-03-07 17:41:34 +0100

    move from main function

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,18 @@
 mod primality;
 mod primefetch;
 
-use primefetch::cli_utils::{check_until, format_strings};
 use primefetch::config::Config;
 
 use clap::Parser;
 
-fn main() {
+use std::io::Result;
+
+use primefetch::cli_utils::primefetch;
+
+fn main() -> Result<()> {
     let config = Config::parse();
 
-    let number: u64 = config.number.expect("No number provided");
+    primefetch(config);
 
-    if !config.count_to {
-        for string in format_strings(number, config.color).iter() {
-            println!("{}", string);
-        }
-    } else {
-        let result = check_until(number);
-        for res in result.get_primes().iter() {
-            println!("{}", res);
-        }
-        eprintln!(
-            "{} primes found until {}.",
-            result.get_count(),
-            result.get_num()
-        );
-    }
+    Ok(())
 }
